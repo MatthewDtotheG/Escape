2d67702425db530209bda950cd4322b278683510
github username <github email address>
2018-03-26 17:52:37 -0400

    create account form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :items, :foreign_key => "seller_id"
   belongs_to :location
   has_many :reviews, :foreign_key => "buyer_id"
   has_many :reservations, through: :items
+
+  validates :name, :username, :email, :age, :location_name, :password, presence: true
+  validates :name, :username, :email, :age, :location_name, :password, uniqueness: true
+
+  def location_name=(name)
+    loc = Location.find_or_create_by(name: name)
+    self.location = loc
+  end
+
+  def location_name
+    self.location
+  end
 
 end
