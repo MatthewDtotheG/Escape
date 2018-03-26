3023d7a1a4985f5787d9b0a7314bdd4e07beb363
github username <github email address>
2018-03-26 16:47:02 -0400

    starting views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :items, :foreign_key :seller_id
+  has_many :items, foreign_key: :seller_id
   belongs_to :location
   has_many :reviews
   has_many :reservations, through: :items
