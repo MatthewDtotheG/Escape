28591cd7a9a1d34769e336a8aedbcda10c851851
github username <github email address>
2018-03-27 14:34:31 -0400

    Started Buyer's Path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'welcome#welcome'
   resources :reservations
   resources :reviews
   resources :locations
-  resources :items
+  # resources :items, except: :index
   resources :users
   # resources :users do
   #   resources :items
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
   post "sessions", to: "sessions#create", as: "sessions"
+  get "items/available", to: "items#index", as: "items_available"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
